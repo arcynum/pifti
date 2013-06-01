151afc31a3856a81d6577a71aa831869e564625a
Chris Hamilton <arcynum@gmail.com>
2013-06-01 21:38:22 +1000

    Added User Model to the System. Also populated the admin site with a custom save option (for the user model)

--- imageboard/admin.py
+++ imageboard/admin.py
@@ -2,11 +2,25 @@ from imageboard.models import Post, Comment
 from django.contrib import admin
 
 class CommentInline(admin.TabularInline):
-  model = Comment
-  extra = 1
+  	model = Comment
+  	fields = ('body',)
+  	extra = 1
 
 class PostAdmin(admin.ModelAdmin):
-  fields = ['title', 'body']
-  inlines = [CommentInline]
+  	fields = ('title', 'body',)
+  	inlines = [CommentInline]
+
+	def save_model(self, request, obj, form, change): 
+	    obj.user = request.user
+	    obj.save()
+
+	def save_formset(self, request, form, formset, change): 
+	    if formset.model == Comment:
+	        instances = formset.save(commit = False)
+	        for instance in instances:
+	            instance.user = request.user
+	            instance.save()
+	    else:
+	        formset.save()
 
 admin.site.register(Post, PostAdmin)
--- imageboard/models.py
+++ imageboard/models.py
@@ -1,15 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Post(models.Model):
-  title = models.CharField(max_length = 200)
-  body = models.TextField()
+	user = models.ForeignKey(User)
+	title = models.CharField(max_length = 200)
+	body = models.TextField()
   
-  def __unicode__(self):
-    return self.title
+  	def __unicode__(self):
+  		return self.title
   
 class Comment(models.Model):
-  post = models.ForeignKey(Post)
-  body = models.TextField()
-  
-  def __unicode__(self):
-    return 'Comment: %s' % self.id
+	user = models.ForeignKey(User)
+ 	post = models.ForeignKey(Post)
+ 	body = models.TextField()
