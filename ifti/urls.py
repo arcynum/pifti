4c332d7a22f79766caa0d6d4ba3c36cddce7a4f5
Chris Hamilton <arcynum@gmail.com>
2016-02-10 20:51:49 +1000

    Adjusted the URL scheme

--- ifti/urls.py
+++ ifti/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'', include('imageboard.urls')),
+    url(r'^$', include('imageboard.urls')),
     url(r'^admin/', admin.site.urls)
 ]
 
