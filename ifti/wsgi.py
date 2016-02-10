0583b519b272accb280e3330f6d36db68196a50c
Chris Hamilton <arcynum@gmail.com>
2016-02-10 19:48:11 +1000

    Added WSGI to the path

--- ifti/wsgi.py
+++ ifti/wsgi.py
@@ -13,8 +13,9 @@ middleware here, or combine a Django application with an application of another
 framework.
 
 """
-import os
+import os, sys
 
+sys.path.append('/var/www/pifti/')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ifti.settings")
 
 # This application object is used by any WSGI server configured to use this
